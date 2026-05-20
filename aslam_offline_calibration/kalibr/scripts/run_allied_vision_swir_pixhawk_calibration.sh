cfba408cc3a165e853fcaf8ddb94eff750a6bbf4
fabio.castellini <fabio.castellini.leonardo@gmail.com>
2026-05-20 12:49:10 +0200

    Moved all catkin packages to a src folder

--- aslam_offline_calibration/kalibr/scripts/run_allied_vision_swir_pixhawk_calibration.sh
+++
@@ -1,5 +0,0 @@
-rosrun kalibr kalibr_calibrate_imu_camera \
-  --bag /data/20260519_170307_bolzano_rope_bringup/20260519_170307_kalibr_rope_bolzano_rosbag/20260519_170307_kalibr_rope_bolzano_rosbag_0.bag \
-  --cam ../config/fb3-2026-05/allied_vision_swir_pixhawk_camchain.yaml \
-  --imu ../config/fb3-2026-05/pixhawk_calibration.yaml \
-  --target ../config/fb3-2026-05/calibration_target.yaml
---
+++ scripts/run_basler51gc_krio_calibration.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+set -e
+
+# --- Files ---
+BAG="/data/20260519_170959_bolzano_kalibr_basler/20260519_170959_kalibr_rope_bolzano_basler.bag"
+
+CAM="/workspace/config/fb3-2026-05/basler51gc_pixhawk_camchain.yaml"
+IMU="/workspace/config/fb3-2026-05/krio_calibration.yaml"
+TARGET="/workspace/config/calibration_target.yaml"
+
+# --- Environment ---
+source /opt/ros/noetic/setup.bash
+source /workspace/devel/setup.bash
+
+# --- Run ---
+rosrun kalibr kalibr_calibrate_imu_camera \
+  --bag "$BAG" \
+  --cam "$CAM" \
+  --imu "$IMU" \
+  --target "$TARGET"
